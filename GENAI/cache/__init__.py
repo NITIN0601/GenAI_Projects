d835f68fb3ee60fa29f18495690fe71dc63dc8da
Your Name <dmnitin97@gmail.com>
2025-11-26 06:56:52 -0500

    inital commit

--- GENAI/cache/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Cache package initialization."""
-
-from .redis_cache import RedisCache, get_redis_cache
-
-__all__ = ['RedisCache', 'get_redis_cache']
--- GENAI/scrapers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Scrapers package initialization."""
-
-from .pdf_scraper import EnhancedPDFScraper
-from .metadata_extractor import MetadataExtractor
-
-__all__ = ['EnhancedPDFScraper', 'MetadataExtractor']
